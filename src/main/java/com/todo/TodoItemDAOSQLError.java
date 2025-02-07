8bdbc83dac6635a461f7a4508fd529931c68c9af
JacobTurner1801 <jacobmturner@gmail.com>
2025-02-07 21:06:39 +0000

    add custom error (basically wraps SQLException)

---
+++ src/main/java/com/todo/TodoItemDAOSQLError.java
@@ -0,0 +1,12 @@
+package com.todo;
+
+import java.sql.SQLException;
+
+public class TodoItemDAOSQLError {
+    public TodoItemDAOSQLError(String message, SQLException se) {
+        System.err.println("ERROR: " + message);
+        System.err.println(se.getSQLState() + " " + se.getErrorCode());
+        System.err.println(se.getMessage());
+        se.printStackTrace();
+    }
+}
