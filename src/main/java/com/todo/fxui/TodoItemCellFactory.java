c13f66a057c8a4f0863f52c7181dac9782c6e181
JacobTurner1801 <jacobmturner@gmail.com>
2025-02-13 16:10:51 +0000

    user can now remove items

--- src/main/java/com/todo/fxui/TodoItemCellFactory.java
+++ src/main/java/com/todo/fxui/TodoItemCellFactory.java
@@ -3,16 +3,20 @@ package com.todo.fxui;
 import com.todo.TodoItem.TodoItem;
 import com.todo.TodoItem.TodoItemDAO;
 
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
-
+/**
+ * Creates the ListCells for a todoitem.
+ */
 public class TodoItemCellFactory extends ListCell<TodoItem> {
     private TodoItemDAO dao;
 
@@ -77,11 +81,15 @@ public class TodoItemCellFactory extends ListCell<TodoItem> {
                     }
                 };
                 new Thread(updateTask).start();
-                todoItemService.restart();
+                this.todoItemService.restart();
                 this.getListView().refresh(); // force a refresh of the list view
             });
 
-            HBox cellContent = new HBox(checkbox, taskL);
+            Button removeButton = new Button("remove");
+
+            removeButton.setOnAction(event -> handleRemove(item));
+
+            HBox cellContent = new HBox(checkbox, taskL, removeButton);
             cellContent.setAlignment(Pos.CENTER);
             cellContent.setSpacing(10);
             cellContent.setPrefWidth(300);
@@ -95,4 +103,34 @@ public class TodoItemCellFactory extends ListCell<TodoItem> {
         }
     }
 
+    /**
+     * removes item from database and updates UI. 
+     */
+    private void handleRemove(TodoItem item) {
+        if (item != null) {
+            System.out.println("removing item: " + item.getId() + "\n" + item.getTask());
+            Task<Void> removeTask = new Task<Void>() {
+                @Override
+                protected Void call() throws Exception {
+                    dao.deleteTodoItem(item.getId());
+                    return null;
+                }
+
+                @Override
+                protected void failed() {
+                    Throwable e = getException();
+                    System.out.println("Error removing " + item.getTask() + " " + e.getMessage());
+                    e.printStackTrace();
+                }
+            };
+            removeTask.setOnSucceeded(e -> {
+                Platform.runLater(() -> {
+                    todoItems.remove(item);
+                    this.getListView().refresh();
+                    this.todoItemService.restart();
+                });
+            });
+            new Thread(removeTask).start();
+        }
+    }
 }
